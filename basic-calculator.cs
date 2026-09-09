100c3aedceae73d89c8b6bad62ebc876f0927d7c
JarrodSeville <jarrodseville403@gmail.com>
2026-09-09 18:21:53 +0100

    Commit-5: Input validation
    Ensures the user has entered an operation and number where the program asks them to. If the user has not entered a valid input, an error message will be displayed.

--- basic-calculator.cs
+++ basic-calculator.cs
@@ -12,47 +12,63 @@ namespace basic_calc
         {
             // This asks the user for the two numbers and the operator
             Console.Write("Enter the first number: ");
-            double num1 = double.Parse(Console.ReadLine());
+            string input1 = Console.ReadLine();
 
             Console.Write("Enter the operator (+, -, *, /): ");
             char op = Console.ReadLine()[0];
 
             Console.Write("Enter the second number: ");
-            double num2 = double.Parse(Console.ReadLine());
-
-            // When the user wants to add the numbers together
-            if (op == '+')
+            string input2 = Console.ReadLine();
+            
+            // This validates whether the inputs are valid numbers
+            if (double.TryParse(input1, out double num1) && double.TryParse(input2, out double num2))
             {
-                double ans = num1 + num2;
-                Console.WriteLine($"{num1} + {num2} = {ans}");
-            }
-
-            // When the user wants to subtract the numbers
-            if (op == '-')
-            {
-                double ans = num1 - num2;
-                Console.WriteLine($"{num1} - {num2} = {ans}");
-            }
-
-            // When the user wants to multiply the numbers
-            if (op == '*')
-            {
-                double ans = num1 * num2;
-                Console.WriteLine($"{num1} * {num2} = {ans}");
-            }
-
-            // When the user wants to divide the numbers
-            if (op == '/')
-            {   if (num2 != 0)
+                // When the user wants to add the numbers together
+                if (op == '+')
                 {
-                    double ans = num1 / num2;
-                    Console.WriteLine($"{num1} / {num2} = {ans}");
+                    double ans = num1 + num2;
+                    Console.WriteLine($"{num1} + {num2} = {ans}");
                 }
-                else
+
+                // When the user wants to subtract the numbers
+                if (op == '-')
                 {
-                    Console.WriteLine("Error: Division by zero is not allowed.");
+                    double ans = num1 - num2;
+                    Console.WriteLine($"{num1} - {num2} = {ans}");
                 }
-            }  
+
+                // When the user wants to multiply the numbers
+                if (op == '*')
+                {
+                    double ans = num1 * num2;
+                    Console.WriteLine($"{num1} * {num2} = {ans}");
+                }
+
+                // When the user wants to divide the numbers
+                if (op == '/')
+                {
+                    if (num2 != 0)
+                    {
+                        double ans = num1 / num2;
+                        Console.WriteLine($"{num1} / {num2} = {ans}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                }
+
+                // User validation for the operator
+                if (op != '+' || op != '-' || op != '*' || op != '/')
+                {
+                    Console.WriteLine("Error: Invalid operator. Please use +, -, *, or /.");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid input. Please enter valid numbers.");
+            }
         }
     }
 }
