607f3d21d801ab344ebdd9e03abd84c75fefb52e
JarrodSeville <jarrodseville403@gmail.com>
2026-09-09 17:33:21 +0100

    Commit-1: User input
    Asks the user for the two numbers and the operator.

---
+++ basic-calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_calc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // This asks the user for the two numbers and the operator
+            Console.Write("Enter the first number: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the operator (+, -, *, /): ");
+            char op = Console.ReadLine()[0];
+
+            Console.Write("Enter the second number: ");
+            double num2 = double.Parse(Console.ReadLine());
+        }
+    }
+}
